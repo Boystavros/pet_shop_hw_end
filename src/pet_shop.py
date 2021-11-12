c23d07dd449a296df5348cb25f9594c31375ec65
Sacha Ponniah <sponniah812@gmail.com>
2021-11-12 14:29:05 +0000

    halfway through integration test

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -56,4 +56,15 @@ def customer_can_afford_pet(customer, pet):
     else:
         return False
 
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if find_pet_by_name:
+        if customer_can_afford_pet(customer, pet):
+            add_pet_to_customer(customer, pet)
+            remove_pet_by_name(pet_shop, pet)
+            increase_pets_sold(pet_shop, 1)
+            remove_customer_cash(customer, pet["price"])
+
+
+
+
 
