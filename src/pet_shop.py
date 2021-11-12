742b10eba8a95ecf56e7d55dd1425ac4dacb20fa
Sacha Ponniah <sponniah812@gmail.com>
2021-11-12 13:26:35 +0000

    8th test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,5 +1,4 @@
 # WRITE YOUR FUNCTIONS HERE
-
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
@@ -17,4 +16,11 @@ def increase_pets_sold(pet_shop, sold_num):
 
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    is_breed_list = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            is_breed_list.append(pet)
+    return is_breed_list
 
