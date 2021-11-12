aff8748ad962d4c7455ada6cf964d9f1518c77a0
Sacha Ponniah <sponniah812@gmail.com>
2021-11-12 14:00:00 +0000

    17th test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -45,4 +45,7 @@ def remove_customer_cash(customer, amount):
 def get_customer_pet_count(customer):
     return len(customer["pets"])
 
+def  add_pet_to_customer(customer, pet):
+    customer["pets"].append(pet)
+
     
