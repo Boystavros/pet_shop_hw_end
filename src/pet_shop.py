18a173b09e57525ddaa3221b7ac03dccf7b8fabc
Sacha Ponniah <sponniah812@gmail.com>
2021-11-12 12:45:34 +0000

    6th test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -12,3 +12,5 @@ def add_or_remove_cash(pet_shop, amount):
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
+def increase_pets_sold(pet_shop, sold_num):
+    pet_shop["admin"]["pets_sold"] += sold_num
